e9ad64fa4ef46da1851b0e1aa5e593faaf6e52e1
Smart Coder <smartcoder415@gmail.com>
2019-11-24 14:21:26 -0500

    Heroku deploy.

--- scripts/set-heroku-config.js
+++ scripts/set-heroku-config.js
@@ -5,4 +5,5 @@ const getConfigVariables = require('./getConfigVariables')
 const output = getConfigVariables()
 
 const deployFile = path.resolve(__dirname, './deploy.sh')
+console.log(output)
 const deployProcess = shell.exec(`heroku config:set ${output}`)
