7dffea94fc9dd685e0527cbc040c01bdd81905f5
Smart Coder <smartcoder415@gmail.com>
2019-11-23 19:01:11 -0500

    Heroku deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Setting env variables in Heroku: ${1}\n"
+echo "Setting env variables in Heroku: ${1}"
 heroku config:add ${1}
 
-echo "Committing and pushing changes.\n"
+echo "Committing and pushing changes."
 git add .
 git commit -m "Heroku deploy."
 git push heroku master
