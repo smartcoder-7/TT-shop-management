f2dda359a8e897f07b95e3c8e200d4927de7900a
Smart Coder <smartcoder415@gmail.com>
2019-12-13 01:09:07 -0500

    Reflect premium

--- src/components/Reservations/index.js
+++ src/components/Reservations/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { INTERVAL_MS, formatTime } from "util/getPodSessions"
+import RateLabel from 'components/RateLabel'
 import parseSessionId from 'util/parseSessionId'
 import getDateParts from "util/getDateParts"
+import getSessionRate from 'util/getSessionRate'
 import styles from './styles.scss'
 
 const formatInfo = reservation => {
@@ -24,8 +26,13 @@ const ReservationRange = ({
   const first = parseSessionId(firstSessionId)
   const last = parseSessionId(lastSessionId)
 
+  const premium = reservations[0].isPremium
+
   return (
     <div className={styles.reservation}>
+      {premium && <span className={styles.premiumLabel}>
+        <RateLabel rate={{ displayName: 'Premium' }} />
+      </span>}
       <label>
         {first.formattedDate}
       </label>
@@ -48,7 +55,9 @@ const Reservations = ({
   sortedReservations.forEach((res, i) => {
     const prevRes = sortedReservations[i - 1]
     const isContiguous =
-      prevRes && res.reservationTime <= prevRes.reservationTime + INTERVAL_MS
+      prevRes &&
+      res.reservationTime <= prevRes.reservationTime + INTERVAL_MS &&
+      prevRes.isPremium === res.isPremium
 
     if (isContiguous) {
       ranges[ranges.length - 1].push(res)
