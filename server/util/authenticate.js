1a6ecf5637f0d56b78ae166574562521f0d2a98d
Smart Coder <smartcoder415@gmail.com>
2019-12-17 17:57:58 +0100

    Show details in admin

--- server/util/authenticate.js
+++ server/util/authenticate.js
@@ -3,7 +3,16 @@ const { auth } = require('./firebase')
 const IS_OFFLINE = process.env.IS_OFFLINE === "true"
 
 const ADMIN = {
-  "sXNTtawm7sME2ydcwBDoSin4TS92": true
+  // itschristinecha+5@gmail.com
+  "sXNTtawm7sME2ydcwBDoSin4TS92": true,
+  // max@pingpod.com
+  "Q63QHeYrxUbRfMp4znqEDFosPJn2": true,
+  // maxkogler@gmail.com
+  "Z7GutoomyCMYAdsjxoU72UC7BgH2": true,
+  // ernesto@pingpod.com
+  "RmObH5fZlheisbCxni7FnJbhxUi2": true,
+  // david@pingpod.com
+  "uwiOkNc361bwpDX1oZxPI5L3kCR2": true
 }
 
 const authenticate = fn => (req, res) => {
