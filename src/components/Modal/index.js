e198ec58a36a18716b1f54e80bc186fdea7aab2a
Smart Coder <smartcoder415@gmail.com>
2019-12-21 07:57:35 +0100

    Fix Safari

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -9,8 +9,8 @@ const Modal = ({ children, isActive }) => {
   return (
     <>
       <div className={styles.modal} data-active={isActive} onClick={onClose}>
-        <div data-bg-color className={styles.modalContent} data-row onClick={(e) => e.stopPropagation()}>
-          <div className={styles.modalInner} data-col="12">
+        <div className={styles.modalContent} data-row onClick={(e) => e.stopPropagation()}>
+          <div data-bg-color className={styles.modalInner} data-col="12">
             {children}
             <div className={styles.close} onClick={onClose}>✕</div>
           </div>
