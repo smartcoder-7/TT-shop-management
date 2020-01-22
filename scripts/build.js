ecd902b406064fa260bd6a184034cf2f01a20f65
Smart Coder <smartcoder415@gmail.com>
2020-01-21 19:31:22 -0500

    Trigger staging deploy.

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,8 @@ console.log('node_env', process.env.NODE_ENV)
 console.log('firebase', process.env.FIREBASE_API_KEY)
 
 shell.exec(`
-  NODE_ENV=${process.env.NODE_ENV}
-  STRIPE_PUBLISHABLE_KEY=${process.env.STRIPE_PUBLISHABLE_KEY}
-  FIREBASE_API_KEY=${process.env.FIREBASE_API_KEY}
-  webpack
+NODE_ENV=${process.env.NODE_ENV}\n
+STRIPE_PUBLISHABLE_KEY=${process.env.STRIPE_PUBLISHABLE_KEY}\n
+FIREBASE_API_KEY=${process.env.FIREBASE_API_KEY}\n
+webpack
 `)
