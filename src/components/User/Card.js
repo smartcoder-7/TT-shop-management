6312f8cc81354f02d94153087d8eafe92956a4ff
Smart Coder <smartcoder415@gmail.com>
2020-02-11 23:16:21 -0500

    Add more credit card svgs

--- src/components/User/Card.js
+++ src/components/User/Card.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import visaSvg from 'payment-icons/min/flat/visa.svg'
+import defaultSvg from 'payment-icons/min/flat/default.svg'
 import mastercardSvg from 'payment-icons/min/flat/mastercard.svg'
+import amexSvg from 'payment-icons/min/flat/amex.svg'
+import discoverSvg from 'payment-icons/min/flat/discover.svg'
+import dinersSvg from 'payment-icons/min/flat/diners.svg'
+import unionpaySvg from 'payment-icons/min/flat/unionpay.svg'
 
 import styles from './styles.scss'
 
 const ICONS = {
   Visa: visaSvg,
   MasterCard: mastercardSvg,
+  'American Express': amexSvg,
+  'Diners Club': dinersSvg,
+  'Discover': discoverSvg,
+  'UnionPay': unionpaySvg,
 }
 
 const Card = ({
@@ -26,7 +35,7 @@ const Card = ({
         <span
           className={styles.cardBrand}
           data-type={brand.toUpperCase()}
-          dangerouslySetInnerHTML={{ __html: ICONS[brand] }}
+          dangerouslySetInnerHTML={{ __html: ICONS[brand] || defaultSvg }}
         />
       )}
 
